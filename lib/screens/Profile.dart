f89203b596ccbcf143fdd8dea9848091284e1d69
mohammadkaif <nazimsahun@gmail.com>
2023-06-03 00:07:08 +0530

    1 commit

---
+++ lib/screens/Profile.dart
@@ -0,0 +1,11 @@
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Profile"),
+    );
+
+  }
+}
